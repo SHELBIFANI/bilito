8cbe15928a51f16ab0cd981378c21815fc12ddb2
MohsenArvan <F.n.rayat@gmail.com>
2023-10-30 20:43:43 +0330

    create model and controller for city, airline ,flight

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -13,9 +13,9 @@ class AuthenticationTest extends TestCase
     public function test_users_can_authenticate_using_the_login_screen(): void
     {
         $user = User::factory()->create();
-
+        
         $response = $this->post('/login', [
-            'email' => $user->email,
+            'mobile' => $user->mobile,
             'password' => 'password',
         ]);
 
