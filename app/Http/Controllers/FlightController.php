fa058f3b078b5b79d9eb1463704e51191034e792
MohsenArvan <F.n.rayat@gmail.com>
2023-11-11 22:20:23 +0330

    add request filter in Flight Controller

--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -10,7 +10,7 @@ class FlightController extends Controller
     public function show(Request $request)
     {
         
-        
+        $departure = $request->input('departure');
         $origin = $request->input('origin');
         $destination = $request->input('destination');
         $number_of_passenger = $request->input('number_of_passenger');
@@ -22,13 +22,35 @@ class FlightController extends Controller
         $result = Flight::query()
         ->where('origin_id', $origin)
         ->where('destination_id', $destination)
+        //->where('departure', 'like', "%$departure%")
+        ->whereDate('departure', '=' ,$departure)
         ->where('capacity', '>=', $number_of_passenger)
         ->get();
         
+        if($request->input('price')){
+            $result = $result->where('price', '<=', $request->input('price'));
+        }
+
+        if($request->has('time')) {
+            $time = $request->input('time');
+            // $flights = Flight::get();
+            // foreach($flights as $flight){
+            //     $dateTime = date('H:i:s', strtotime($flight->daparture));
+            // }
+            // dd($dateTime);
+            // $result = $result->whereTime('departure', '=', '23:00:00');
+            // $result = $result->whereTime('departure', '=', '23:00:00');
+        }
+
         if($result->isEmpty()){
             return response(['message' => 'No flights found'], 404); 
             
         }
-        return $result;
+
+        return response()->json($result);
+        //return $request
+        //create filter sorting
+        //add model Airline in this page
+        
     }
 }
