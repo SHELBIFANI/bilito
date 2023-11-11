8efbaf36f71cb2668f65091b0d7cbcd985bb06ba
MohsenArvan <F.n.rayat@gmail.com>
2023-11-11 16:08:01 +0330

    create Flight Controller and set query Bilder for input search

--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -2,47 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\City;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class FlightController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function show(Request $request)
     {
-        //
+        
+        
+        $origin = $request->input('origin');
+        $destination = $request->input('destination');
+        $number_of_passenger = $request->input('number_of_passenger');
+        
+        $city_origin = City::where('name', $origin)->first();
+        $city_destination = City::where('name', $destination)->first();
+
+        if($city_origin != $city_destination){
+
+            $result = DB::table('flights')
+                    ->where('origin_id', $city_origin->id)
+                    ->where('destination_id', $city_destination->id)
+                    ->where('capacity', '>=', $number_of_passenger)
+                    ->get();
+    
+        }else{
+            return 'There is no flight from '. $city_origin .'to' .$city_destination ;
+        }
+
+        if($result->isEmpty()){
+            
+            return 'There is no flight from '.$origin.' to '.$destination;
+            
+        }else{
+            
+            return $result;
+
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
