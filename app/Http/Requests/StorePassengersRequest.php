fa295d74848ba514c0e14c998b1f2fec7328ec0a
MohsenArvan <f.n.raiat@gmail.com>
2023-11-16 20:31:13 +0330

    Add authentication check to StorePassengersRequest authorization

--- app/Http/Requests/StorePassengersRequest.php
+++ app/Http/Requests/StorePassengersRequest.php
@@ -13,7 +13,7 @@ class StorePassengersRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true;
+        return auth()->check() == true;
     }
 
     /**
