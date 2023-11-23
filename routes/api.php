cb1aa18402de09d7c9aee8ce82415330d210c5e8
Mo Khosh <mskhoshnazar@gmail.com>
2023-11-23 17:49:40 +0330

    move routes to api and remove stateful middleware from api routes

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,39 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\FlightController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\PassengerController;
+use App\Http\Controllers\ProfileController;
+use App\Models\City;
+use App\Models\Flight;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', function () {
+
+    $returnJson = [
+        'cities' => City::get()->map->only(['id', 'name']),
+        'flights' => Flight::with(['origin', 'destination'])->inRandomOrder()->limit(5)->get()->map->only(['id', 'destination', 'origin', 'departure', 'arrival',]),
+    ];
+    return $returnJson;
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::get('/profile/user_orders', [ProfileController::class, 'orders'])->name('profile.orders');
+
+    Route::post('flight/order', [PassengerController::class, 'store'])->name('passemger.store');
+    Route::get('Flight/order/verify', [PassengerController::class, 'verify'])->name('verify');
+
+    Route::get('flight/order/{order}', [OrderController::class, 'show'])->name('order_show');
+});
+
+Route::post('/flight', [FlightController::class, 'show'])->name('flight.show');
+
+Route::get('weekly-prices', [FlightController::class, 'weekly']);
+
+require __DIR__ . '/auth.php';
