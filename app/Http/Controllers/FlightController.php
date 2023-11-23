01bc9cec365d2011c96aee850beaf405ba61ce76
Mo Khosh <mskhoshnazar@gmail.com>
2023-11-23 18:57:33 +0330

    fix typo

--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -16,7 +16,7 @@ class FlightController extends Controller
     {
         $request->validate([
             'origin_id' => 'required|integer|exists:citis,id',
-            'destination_id' => 'required|integer|exists:citis,id',
+            'destination_id' => 'required|integer|exists:cities,id',
             'departure' => 'required|date:Y-m-d',
             'capacity' => 'required|integer',
         ]);
